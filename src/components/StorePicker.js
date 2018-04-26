a32f0f4f975a85cda3929e4c7d7dda3e4ffeddad
Chandler <chndlr.wllm@gmail.com>
2018-04-26 13:07:07 -0500

    store picker functioning, routing in place

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,33 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+  myInput = React.createRef();
+
+  goToStore = (e) => {
+    // stop form from submitting
+    e.preventDefault();
+
+    // get text from input
+    const storeName = this.myInput.value.value;
+
+    // chnage page to store/whatever-they-entered
+    this.props.history.push(`/store/${storeName}`);
+
+  }
+
   render() {
     return (
-      <form className='store-selector'>
+      <form className='store-selector' onSubmit={this.goToStore}>
         <h2>Please enter a store</h2>
-        <input type='text' required placeholder='Store Name' />
+        <input 
+          type='text' 
+          ref={this.myInput}
+          required 
+          placeholder='Store Name' 
+          defaultValue={getFunName()} 
+        />
         <button type='submit'>Visit Store >> </button>
       </form>
     )
