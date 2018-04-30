2b8f2accfa68a3aaeadd364301b4dfb7fcac0549
Chandler <chndlr.wllm@gmail.com>
2018-04-30 14:54:28 -0500

    created addFishForm component and set state with data

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,24 @@ import Inventory from "./Inventory";
 
 class App extends React.Component {
 
+  state = {
+    fishes: {},
+    order: {}
+  };
+
+  addFish = (fish) => {
+    // take a copy of existing state
+    const fishes = { ...this.state.fishes };
+    // add new fish to fishes variable
+    fishes[`fish${Date.now()}`] = fish;
+    // set the new fishes object to state
+    this.setState({
+      fishes: fishes
+    })
+
+  }
+
+
   render() {
 
     return (
@@ -14,7 +32,7 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
         </div>
-        <Inventory />
+        <Inventory addFish={this.addFish} />
         <Order />
       </div>
 
--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,36 +1,37 @@
-import React from 'react';
-import { getFunName } from '../helpers';
+import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
-
+  
   myInput = React.createRef();
 
-  goToStore = (e) => {
-    // stop form from submitting
-    e.preventDefault();
-
-    // get text from input
+  goToStore = event => {
+    
+    // Stop the form from submitting
+    event.preventDefault();
+    
+    // Get the text from that input
     const storeName = this.myInput.value.value;
-
-    // chnage page to store/whatever-they-entered
+    
+    // Change the page to /store/whatever-they-entered
     this.props.history.push(`/store/${storeName}`);
-
-  }
-
+    
+  };
+  
   render() {
     return (
-      <form className='store-selector' onSubmit={this.goToStore}>
-        <h2>Please enter a store</h2>
-        <input 
-          type='text' 
+      <form className="store-selector" onSubmit={this.goToStore}>
+        <h2>Please Enter A Store</h2>
+        <input
+          type="text"
           ref={this.myInput}
-          required 
-          placeholder='Store Name' 
-          defaultValue={getFunName()} 
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
         />
-        <button type='submit'>Visit Store >> </button>
+        <button type="submit">Visit Store →</button>
       </form>
-    )
+    );
   }
 }
 
